942425f1f80ba0c0be4c6d5c3d97be71a79b3d3f
Preston <preston.ng37@gmail.com>
2024-01-11 23:06:22 +0800

    add status feature including winner recognition

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,9 +3,38 @@ import React, { useState, useEffect } from 'react';
 
 function Board({xIsNext, history, setHistory, currentMove, setCurrentMove}) {
     const [tiles, setTiles] = useState(Array(9).fill(null))
-    
+    const [status, setStatus] = useState("X's turn")
+
+    useEffect(() => {
+        const victor = checkWinner(tiles)
+        if(victor){
+           setStatus(`${victor} is the winner!`) 
+        }else{
+            setStatus(`${xIsNext ? 'X' : 'O'}'s turn.`)
+        }
+    }, [tiles]);
+
+    function checkWinner(tiles){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+          for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (tiles[a] && tiles[a] === tiles[b] && tiles[a] === tiles[c]) {
+              return tiles[a];
+            }
+          }
+          return null
+    }    
+
     function update(){
-        console.log("update");
         const updatedHistory = [...history.slice(0, currentMove+1), tiles]
         console.log(updatedHistory);
         setHistory(updatedHistory)
@@ -15,17 +44,17 @@ function Board({xIsNext, history, setHistory, currentMove, setCurrentMove}) {
     function handleClick(i){
         const newTiles = tiles.slice() 
         if(xIsNext){
-            console.log("x");
             newTiles[i] = 'X'
         }else{
-            console.log("o");
             newTiles[i] = 'O'
         }
         setTiles(newTiles)
         update()
     }
+    
     return (
-        <div className="board">
+        <>
+            <h2>{status}</h2>
             <div className="row">
                 <Tile value={tiles[0]} handleClick={() => handleClick(0)}/>
                 <Tile value={tiles[1]} handleClick={() => handleClick(1)}/>
@@ -41,7 +70,7 @@ function Board({xIsNext, history, setHistory, currentMove, setCurrentMove}) {
                 <Tile value={tiles[7]} handleClick={() => handleClick(7)}/>
                 <Tile value={tiles[8]} handleClick={() => handleClick(8)}/>
             </div>
-        </div>
+        </>
       );
 }
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -9,6 +9,26 @@ function Game() {
     const xIsNext = currentMove % 2 === 0
     const currentTiles = history[currentMove]
 
+    function checkWinner(tiles){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+          for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (tiles[a] && tiles[a] === tiles[b] && tiles[a] === tiles[c]) {
+              return tiles[a];
+            }
+          }
+          return null
+    }
+
     return (
         <div>
             <Board 
@@ -18,6 +38,7 @@ function Game() {
                 setHistory={setHistory} 
                 currentMove={currentMove}
                 setCurrentMove={setCurrentMove}
+                checkWinner={checkWinner}
             />
             <div className="game-info">
                 <ol>{}</ol>
